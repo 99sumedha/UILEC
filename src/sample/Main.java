8d489da947a3951b18ce7066af0457c53b7f1637
Sumedha Karunarathna <Sumedha@Kumudithas-MacBook-Pro.local>
2020-02-26 10:56:59 +0530

    Edit main.class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,4 +1,15 @@
 package sample;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.println("Im inside the main methord!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
