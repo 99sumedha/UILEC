0edad937d11df8b04138567df727857ae8e30ec0
Sumedha Karunarathna <Sumedha@Kumudithas-MacBook-Pro.local>
2020-02-26 15:03:24 +0530

    Add controller methords

--- src/lec4/Controller.java
+++ src/lec4/Controller.java
@@ -1,7 +1,6 @@
 package lec4;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 
 
@@ -10,8 +9,24 @@ public class Controller {
     @FXML
     private FlowPane flowPane;
 
-    public void addThirdButton(){
-        Button newBtn = new Button("Third Button");
-        flowPane.getChildren().add(newBtn);
+    public static void addCustomer() {
+        System.out.println("Customer added");
     }
+
+    public static void viewSeats() {
+        System.out.println("View all seats");
+    }
+
+    public static void viewEmptySeats() {
+        System.out.println("View empty seats");
+    }
+
+    public static void deleteBookedSeat() {
+        System.out.println("Delete booked seats");
+    }
+
+    public static void findSeatByCustomerName() {
+        System.out.println("Find seat by customer name");
+    }
+
 }
--- src/lec4/Main.java
+++ src/lec4/Main.java
@@ -7,13 +7,53 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.util.Scanner;
 
 
 public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("I'm inside the main method!");
+        Scanner sc = new Scanner(System.in);
+        Menu:
+        while (true) {
+            System.out.println("A to add customer");
+            System.out.println("V to view all seats");
+            System.out.println("E to view empty seats");
+            System.out.println("D to delete a booked seats");
+            System.out.println("F to find a seat by customer name");
+            System.out.println("Q to quit");
+
+            String option = sc.next();
+            switch (option) {
+                case "A":
+                case "a":
+                    Controller.addCustomer();
+                    break;
+                case "V":
+                case "v":
+                    Controller.viewSeats();
+                    break;
+                case "E":
+                case "e":
+                    Controller.viewEmptySeats();
+                    break;
+                case "D":
+                case "d":
+                    Controller.deleteBookedSeat();
+                    break;
+                case "F":
+                case "f":
+                    Controller.findSeatByCustomerName();
+                    break;
+                case "Q":
+                case "q":
+                    break Menu;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
         launch();
     }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -31,7 +71,6 @@ public class Main extends Application {
 //        button.setStyle("-fx-background-color: blue; -fx-text-fill: white");
 //        label.setLayoutX(100);
 //        label.setLayoutY(200);
-
 
 
 //        anchorPane.getChildren().add(label);
