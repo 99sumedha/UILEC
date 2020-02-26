835a6e46d738539498a0d537502f80a825b75044
Sumedha Karunarathna <Sumedha@Kumudithas-MacBook-Pro.local>
2020-02-26 12:16:03 +0530

    Add button sample.fxml, call a function Button 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
-public class Controler {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
+
+
+public class Controller {
+    @FXML
+    private FlowPane flowPane;
+
+    public void addThirdButton(){
+        Button newBtn = new Button("Third Button");
+        flowPane.getChildren().add(newBtn);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,20 +23,24 @@ public class Main extends Application {
         System.out.println("Inside the start method!");
         Stage stage = primaryStage;
 
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         AnchorPane anchorPane = new AnchorPane();
         Label label = new Label("I'm a label");
         label.setLayoutX(200);
         label.setLayoutY(300);
 
         Button button = new Button("Button");
+        button.setStyle("-fx-background-color: blue; -fx-text-fill: white");
         label.setLayoutX(100);
         label.setLayoutY(200);
 
 
 
-        anchorPane.getChildren().add(label);
-        anchorPane.getChildren().add(button);
-        stage.setScene(new Scene(anchorPane, 500, 500));
+//        anchorPane.getChildren().add(label);
+//        anchorPane.getChildren().add(button);
+        stage.setScene(new Scene(root));
+//        stage.setScene(new Scene(anchorPane, 500, 500));
 
         stage.show();
     }
