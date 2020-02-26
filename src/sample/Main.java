f9e66eacf571bca3d25e39a7491ddaf4ba5bce87
Sumedha Karunarathna <Sumedha@Kumudithas-MacBook-Pro.local>
2020-02-26 11:12:32 +0530

    Set widhth and height main.class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,15 +1,24 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Im inside the main methord!");
+        System.out.println("I'm inside the main method!");
+        launch();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("Inside the start method!");
+        Stage stage = primaryStage;
 
+        AnchorPane anchorPane = new AnchorPane();
+        stage.setScene(new Scene(anchorPane, 500, 500));
+
+        stage.show();
     }
 }
